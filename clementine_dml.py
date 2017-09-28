18c0498921814cd30e6c051c313876fee1f1f55c
Albert Visser <albert@lemoncurry>
2017-09-28 20:52:20 +0200

    gebruik database api i.p.v. string formatting

--- clementine_dml.py
+++ clementine_dml.py
@@ -7,37 +7,34 @@ from banshee_settings import databases
 DB = databases['clementine']
 
 
-def execute_query(db, query):
+def retrieve(db, query, parms):
     "get data from database"
     result = []
     with closing(sqlite3.connect(db)) as conn:
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        ## print(query)
-        for row in cur.execute(query):
+        for row in cur.execute(query, parms):
             result.append({name: row[name] for name in row.keys()})
     return result
 
 
 def list_artists(db):
     "produce list of artists"
-    return execute_query(
-        db, "select distinct artist from songs where unavailable = '0' order by artist;")
+    return retrieve(db, "select distinct artist from songs "
+                    "where unavailable = '0' order by artist;", ())
 
 
 def list_albums(db, artist_name):
     "produce list of albums for artist"
-    return execute_query(
-        db, "select distinct album from songs where artist = '{}' "
-        "and unavailable = '0';".format(artist_name))
+    return retrieve(db, "select distinct album from songs where artist = ? "
+                    "and unavailable = '0';", (artist_name,))
 
 
 def list_tracks(db, artist_name, album_name):
     "produce list of tracks for album"
-    return execute_query(
-        db, "select track, title from songs where artist = '{}' and album = '{}' "
-        "and unavailable = '0' order by disc, track;".format(artist_name,
-                                                             album_name))
+    return retrieve(
+        db, "select track, title from songs where artist = ? and album = ? "
+        "and unavailable = '0' order by disc, track;", (artist_name, album_name))
 
 
 def main():
@@ -53,6 +50,7 @@ def main():
         pprint.pprint(album_list, stream=_out)
         album = album_list[0]['album']
         pprint.pprint(list_tracks(db, artist, album), stream=_out)
+
 
 if __name__ == "__main__":
     main()
