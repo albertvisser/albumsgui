cb14dfe5a021951464dd6f1ab43d144dd99c2663
Albert Visser <albert.visser@gmail.com>
2023-06-10 12:43:23 +0200

    hoofdimport gewijzigd m.h.o.o. andere functienamen

--- apps/strawberry_dml.py
+++ apps/strawberry_dml.py
@@ -1,6 +1,6 @@
 """dml for Strawberry database
 """
-from apps.clementine_dml import (databases, list_artists, list_albums, list_album_covers,
-                                 list_tracks_for_artist, list_tracks_for_album)
+from apps.clementine_dml import (databases, get_artists_lists, get_albums_lists, get_album_cover,
+                                 get_tracks_lists)
 # from .banshee_settings import databases
 DB = databases['strawberry']
